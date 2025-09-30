2c331d28c441f8f6a25dc61eaa24a2fe52b98773
rahul235004 <23rahul54@gmail.com>
2025-09-30 22:58:26 +0530

    backend wake-up ping'

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
 import { AuthProvider } from './auth/AuthContext'
+import { useEffect } from 'react';
 
 
 function App() {
+  useEffect(() => {
+    const API_URL = process.env.REACT_APP_API_URL || import.meta.env.VITE_API_URL;
+    fetch(`${API_URL}/api/products?limit=1`).catch(() => {});
+  }, []);
   return (
-  <BrowserRouter>
+    <BrowserRouter>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
